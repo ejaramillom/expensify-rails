02188e7c8edd7a3104cf60faacfb5b7b0fa01bdd
ejaramillom <ejaramilo@hotmail.com>
2020-05-09 13:18:54 -0500

    added 5 unitary tests for expense model

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'sqlite3', '~> 1.4'
   gem 'rspec-rails', '~> 4.0.0'
+  gem 'minitest-reporters'
 end
 
 group :development do
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -3,7 +3,55 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Expense concept can't be blank " do
+    @expense = Expense.create()
+    assert_not @expense.save
+  end
+
+  test "Expense value can't be blank " do
+    @expense = Expense.create(
+      type_id: rand(1..Type.count),
+      category_id: rand(1..Category.count),
+      concept: Faker::TvShows::RickAndMorty.quote,
+      expense_date: Faker::Time.between(from: Date.today - 240, to: Date.today),
+      created_at: Faker::Time.between(from: Date.today - 20, to: Date.today),
+    )
+    assert_not @expense.save
+  end
+
+  test "Expense date can't be blank " do
+    @expense =  Expense.create(
+      type_id: rand(1..Type.count),
+      category_id: rand(1..Category.count),
+      concept: Faker::TvShows::RickAndMorty.quote,
+      value: Faker::Number.between(from: 1, to: 5000000)
+    )
+    assert_not @expense.save
+  end
+
+  test "Expense type can't be blank " do
+    @expense = Expense.create(
+      category_id: rand(1..Category.count),
+      concept: Faker::TvShows::RickAndMorty.quote,
+      expense_date: Faker::Time.between(from: Date.today - 240, to: Date.today),
+      created_at: Faker::Time.between(from: Date.today - 20, to: Date.today),
+      value: Faker::Number.between(from: 1, to: 5000000)
+    )
+    assert_not @expense.save
+  end
+
+  test "Date_cannot_be_in_the_future should avoid wrong date info " do
+    @expense = Expense.create(
+      type_id: rand(1..Type.count),
+      category_id: rand(1..Category.count),
+      concept: Faker::TvShows::RickAndMorty.quote,
+      expense_date: Date.today + 1,
+      created_at: Faker::Time.between(from: Date.today - 20, to: Date.today),
+      value: Faker::Number.between(from: 1, to: 5000000)
+    )
+    @expense.date_cannot_be_in_the_future
+    assert_not @expense.save
+  end
+
 end
